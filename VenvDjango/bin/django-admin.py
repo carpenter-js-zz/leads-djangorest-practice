42f1b4bffe8f5696926e47873faf0652a474d376
carpenter-js <carpenterbarrett@gmail.com>
2019-04-01 20:10:52 -0600

    setup models, serializers, urls

---
+++ VenvDjango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/barrettcarpenter/Desktop/ambassador_challenge/practice_app/VenvDjango/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
